66354094b7ef1124cfa002d9bf435ca6efda02f4
BAhaa Enab <bahaenab@gmail.com>
2025-07-20 14:38:29 +0300

    ssss

--- TeamManager/server/routes/player.routes.js
+++ TeamManager/server/routes/player.routes.js
@@ -1,11 +1,8 @@
-module.exports = function (app) {
-  app.get('/api', (req, res) => {
-    res.json({ message: "API is working fine!" });
-  });
-
-  // باقي الراوترات كما هي
-  app.get('/api/:id', PlayerController.getPlayer);
-  app.post('/api/new', PlayerController.createPlayer);
-  app.patch('/api/edit/:id', PlayerController.updatePlayer);
-  app.delete('/api/delete/:id', PlayerController.deletePlayer);
+const PlayerController = require('../controllers/player.controller');
+module.exports= function (app){
+    app.get('/api', PlayerController.getAllPlayer);
+    app.get('/api/:id',PlayerController.getPlayer);
+    app.post('/api/new',PlayerController.createPlayer);
+    app.patch('/api/edit/:id',PlayerController.updatePlayer);
+    app.delete('/api/delete/:id',PlayerController.deletePlayer)
 }
